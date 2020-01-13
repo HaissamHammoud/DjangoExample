9aaad0f988ac5a88cefe53db33fd383b6cc05f1a
Haissam <haissam_fawaz_4@hotmail.com>
2020-01-13 20:24:42 -0300

    configurando ainda

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -81,8 +81,8 @@ WSGI_APPLICATION = 'trydjango.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-DATABASE_URL  = os.environ['DATABASE_URL']
-conn = psycopg2.connect(DATABASE_URL , sslmode = 'required')
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -135,4 +135,3 @@ USE_TZ = True
 STATIC_URL = os.path.join(BASE_DIR, "static/")
 
 django_heroku.settings(locals())
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
