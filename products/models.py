1614598e44c9dd28eb5e6e18c9fd1de33662f5f2
Haissam <haissam_fawaz_4@hotmail.com>
2020-01-13 18:38:22 -0300

    configuração

--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Product(models.Model):
     title       =   models.CharField(max_length = 120)
     description =   models.TextField(blank = True, null = True)
-    price       =   models.DecimalField(decimal_places = 100, max_digits = 2)
+    price       =   models.DecimalField(decimal_places = 100, max_digits = 1000)
     summary     =   models.TextField(default = 'this is cool', null = True)
     featured    =   models.BooleanField(default = True)
     stock       =   models.IntegerField(default = 0)
